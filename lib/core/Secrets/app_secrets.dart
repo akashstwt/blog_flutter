641aa5aff2ed9a3f9fd4ff4690b260f5885228f2
akashstwt <akashpatel115347@gmail.com>
2024-08-24 02:57:29 +0530

    database updated

--- lib/core/Secrets/app_secrets.dart
+++ lib/core/Secrets/app_secrets.dart
@@ -1,4 +1,5 @@
 class AppSecrets {
-  static const supabaseUrl = 'https://cjqklwmkwxmwnjmyjqtv.supabase.co';
-  static const supabaseAnonKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqcWtsd21rd3htd25qbXlqcXR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc2NTI1NjgsImV4cCI6MjAzMzIyODU2OH0.YltpUGAJajncf18l91sN337FZ2iG1UAjhhSArFRBHR8';
+  static const supabaseUrl = 'https://vvxtkfucpjbklvtcmvhf.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2eHRrZnVjcGpia2x2dGNtdmhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ0NDcwODYsImV4cCI6MjA0MDAyMzA4Nn0.AClLcHPqs61gXqWNKr8NliyKy6RUCsueR8E_i6q8Z84';
 }
