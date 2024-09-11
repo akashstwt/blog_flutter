4331c7ef86de5dac35a728baca2ebee9db2abd00
akashstwt <akashpatel115347@gmail.com>
2024-09-11 16:49:12 +0530

    4.49.50

--- lib/features/blog/presentation/pages/add_new_blog_page.dart
+++ lib/features/blog/presentation/pages/add_new_blog_page.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:blog_app/core/theme/app_pallete.dart';
 import 'package:blog_app/core/utils/pick_image.dart';
-import 'package:blog_app/features/auth/blog/presentation/widgets/blog_editor.dart';
+import 'package:blog_app/features/blog/presentation/widgets/blog_editor.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
