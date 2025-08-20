7289cf0cb2623602fc70405ae0a151fe372cb73f
akashstwt <akashpatel115347@gmail.com>
2025-08-20 20:56:06 +0530

    dependency updated

--- lib/core/Secrets/app_secrets.dart
+++ lib/core/Secrets/app_secrets.dart
@@ -1,5 +1,5 @@
 class AppSecrets {
-  static const supabaseUrl = 'https://vvkrsrnoassmwllasmsr.supabase.co';
+  static const supabaseUrl = 'https://zgvjxllfkidxemjrvfrj.supabase.co';
   static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2a3Jzcm5vYXNzbXdsbGFzbXNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ1MjE2OTEsImV4cCI6MjA0MDA5NzY5MX0.1PCqxhQ2i3YONp1HCA6uxaEe13hszzituO0Skh5LoeY';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpndmp4bGxma2lkeGVtanJ2ZnJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2OTg0MjMsImV4cCI6MjA3MTI3NDQyM30.TfXklqbQrO8wkaDlXIxKlagENrK84EV0YbjJHxReKVk';
 }
